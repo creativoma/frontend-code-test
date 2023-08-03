fb0dac86b62982e2ffd66f9f06049ec526a3974f
Mariano Álvarez <ma.marianoalvarez@gmail.com>
2023-08-03 16:13:04 +0200

    Style adjustments

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,8 +1,10 @@
 import React from "react";
+import LogoGenially from "./LogoGenially";
 
 function Toolbar() {
   return (
     <div className="toolbar">
+      <LogoGenially />
       <button>Add Box</button>
       <button>Remove Box</button>
       <input type="color" />
